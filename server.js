e1c2d60c8c5d9eed2a4a9428383737c812e96df4
User Name <jsalcedo19@gmail.com>
2024-11-24 14:42:06 -0500

    created .env file connected to MongoDB with mongoose and console logged it.

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 //import modules
+const dotenv = require('dotenv');
+dotenv.config(); // loads the environment variables from .env files
 const express = require('express');
-const app = express();
+const mongoose = require ('mongoose');
 
 
-
-
-
-// get routes 
+const app = express(); //loads express
+//connects to mongoDB using the connection string in .env
+mongoose.connect(process.env.MONGODB_URI); 
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+  });
+//routes 
 app.get("/", async (req, res) => {
     res.render("index.ejs");
 })
 
 
 app.listen(3000, () => {
-    console.log("listening on localhost: port 3000")
+    console.log("listening on port localhost:3000")
 });
 
